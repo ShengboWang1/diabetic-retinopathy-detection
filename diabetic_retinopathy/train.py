78e1384a3a6bba4aa90e41eac694e762c5c8aced
Shengbo Wang <st169852@stud.uni-stuttgart.de>
2020-11-24 13:11:01 +0100

    checkpoint and summary writer

--- diabetic_retinopathy/train.py
+++ diabetic_retinopathy/train.py
@@ -7,9 +7,16 @@ class Trainer(object):
     def __init__(self, model, ds_train, ds_val, ds_info, run_paths, total_steps, log_interval, ckpt_interval):
         # Summary Writer
         # ....
+        self.train_loss_summary_writer = tf.summary.create_file_writer("./train_loss")
+        self.train_accuracy_summary_writer = tf.summary.create_file_writer("./train_loss")
+        self.test_loss_summary_writer = tf.summary.create_file_writer("./test_loss")
+        self.test_accuracy_summary_writer = tf.summary.create_file_writer("./test_loss")
 
         # Checkpoint Manager
         # ...
+        self.checkpoint_path = './checkpoint/train'
+        self.ckpt = tf.train.Checkpoint(optimizer=self.optimizer, model=self.model)
+        self.ckpt_manager = tf.train.CheckpointManager(self.ckpt, self.checkpoint_path, max_to_keep=5)
 
         # Loss objective
         self.loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
@@ -81,7 +88,18 @@ class Trainer(object):
                 self.train_accuracy.reset_states()
 
                 # Write summary to tensorboard
-                # ...
+                # ...train test loss accuracy
+                with self.train_loss_summary_writer.as_default():
+                    tf.summary.scalar('train_loss', self.train_loss.result(), step=self.optimizer.iterations)
+
+                with self.train_accuracy_summary_writer.as_default():
+                    tf.summary.scalar('train_accuracy', self.train_accuracy.result() * 100, step=self.optimizer.iterations)
+
+                with self.test_loss_summary_writer.as_default():
+                    tf.summary.scalar('test_loss', self.test_loss.result(), step=step)
+
+                with self.test_accuracy_summary_writer.as_default():
+                    tf.summary.scalar('test_accuracy', self.test_accuracy.result() * 100, step=step)
 
                 yield self.test_accuracy.result().numpy()
 
@@ -89,9 +107,16 @@ class Trainer(object):
                 logging.info(f'Saving checkpoint to {self.run_paths["path_ckpts_train"]}.')
                 # Save checkpoint
                 # ...
+                save_path = self.ckpt_manager.save()
+                print("Saved checkpoint for step {}: {}".format(int(step), save_path))
+                print("loss {:1.2f}".format(loss.numpy()))
 
             if step % self.total_steps == 0:
                 logging.info(f'Finished training after {step} steps.')
                 # Save final checkpoint
                 # ...
+                save_path = self.ckpt_manager.save()
+                print("Saved checkpoint for final step: {}".format(save_path))
+                print("loss {:1.2f}".format(loss.numpy()))
+
                 return self.test_accuracy.result().numpy()
