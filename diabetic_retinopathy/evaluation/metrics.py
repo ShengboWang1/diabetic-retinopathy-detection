ceab8fe8fedd81bf58fde2ac7516e237e6d02c50
Shengbo Wang <st169852@stud.uni-stuttgart.de>
2020-12-01 13:21:19 +0100

    2 ways to define confusion matrix

--- diabetic_retinopathy/evaluation/metrics.py
+++ diabetic_retinopathy/evaluation/metrics.py
@@ -57,15 +57,15 @@ class ConfusionMatrix(tf.keras.metrics.Metric):
                       dtype = tf.int32
         )
 
-        def update_state(self, *args, **kwargs):
-            # update state
-            confusion_matrix = tf.math.confusion_matrix(y_true, tf.argmax(y_pred, axis=1), num_classes=self.noc)
-            return self.confusion_matrix.assign_add(confusion_matrix)
+    def update_state(self, *args, **kwargs):
+        # update state
+        confusion_matrix = tf.math.confusion_matrix(y_true, tf.argmax(y_pred, axis=1), num_classes=self.noc)
+        return self.confusion_matrix.assign_add(confusion_matrix)
 
-        def result(self):
-            # result
-            diag = tf.linalg.diag_part(self.confusion_matrix)
-            rowsums = tf.math.reduce_sum(self.confusion_matrix, axis=1)
-            result = tf.math.reduce_mean(diag / rowsums, axis=0)
+    def result(self):
+        # result
+        diag = tf.linalg.diag_part(self.confusion_matrix)
+        rowsums = tf.math.reduce_sum(self.confusion_matrix, axis=1)
+        result = tf.math.reduce_mean(diag / rowsums, axis=0)
 
-            return result
+        return result
