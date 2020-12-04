bb7a86065a1d9614d0db056ffee498249accc782
Shengbo Wang <st169852@stud.uni-stuttgart.de>
2020-12-04 19:51:21 +0100

    2 classes

--- diabetic_retinopathy/input_pipeline/datasets.py
+++ diabetic_retinopathy/input_pipeline/datasets.py
@@ -9,6 +9,7 @@ from input_pipeline.preprocessing import preprocess, augment
 def load(name, data_dir):
     if name == "idrid":
         logging.info(f"Preparing dataset {name}...")
+        # 2classes
         train_filename = ["idrid-2train.tfrecord-00000-of-00001"]
         val_filename = ["idrid-2val.tfrecord-00000-of-00001"]
         test_filename = ["idrid-2test.tfrecord-00000-of-00001"]
