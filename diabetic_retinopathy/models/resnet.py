ee9b8668e411fbf5aa0f2ceb71c1d318d59e484b
Shengbo Wang <st169852@stud.uni-stuttgart.de>
2020-12-07 20:37:12 +0100

    update resnet.py

--- diabetic_retinopathy/models/resnet.py
+++ diabetic_retinopathy/models/resnet.py
@@ -91,7 +91,7 @@ class ResNet(k.Model):
 
         self.avgpool = layers.GlobalAveragePooling2D()
         # self.dropout = tf.keras.layers.Dropout(0.2)
-        self.out1 = layers.Dense(num_classes, activation=tf.nn.softmax)
+        self.fc = layers.Dense(num_classes)
 
     def call(self,inputs, training=None):
         # __init__中准备工作完毕；下面完成前向运算过程。
@@ -107,7 +107,7 @@ class ResNet(k.Model):
         x = self.avgpool(x)
         # x = self.dropout(x)
         # [b, 100]
-        x = self.out1(x)
+        x = self.fc(x)
         return x
 
     # 实现 Res Block； 创建一个Res Block
