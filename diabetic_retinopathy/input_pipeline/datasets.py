7fbc96088c8454651698276c6a37bf800d7c5fb4
Shengbo Wang <st169852@stud.uni-stuttgart.de>
2020-12-10 12:57:06 +0100

    change dataset path

--- diabetic_retinopathy/input_pipeline/datasets.py
+++ diabetic_retinopathy/input_pipeline/datasets.py
@@ -43,7 +43,7 @@ def load(name, data_dir):
     elif name == "eyepacs":
         logging.info(f"Preparing dataset {name}...")
         (ds_train, ds_val, ds_test), ds_info = tfds.load(
-            '/home/data/tensorflow_datasets',
+            '/home/data/tensorflow_datasets/diabetic_retinopathy_detection/btgraham-300/3.0.0',
             split=['train', 'validation', 'test'],
             shuffle_files=True,
             with_info=True,
