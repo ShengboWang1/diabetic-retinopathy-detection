4b812916ce1ffa013fac0ea2875bb403023dd2b9
Shengbo Wang <st169852@stud.uni-stuttgart.de>
2020-12-10 14:51:37 +0100

    change dataset path

--- diabetic_retinopathy/main.py
+++ diabetic_retinopathy/main.py
@@ -36,7 +36,7 @@ def main(argv):
     #model = vgg_like(input_shape=[256, 256, 3], n_classes=2)
 
     # model resnet
-    model = resnet50()
+    model = resnet34()
     model.build(input_shape=(16, 256, 256, 3))
     model.summary()
     checkpoint = tf.train.Checkpoint(myModel=model)
