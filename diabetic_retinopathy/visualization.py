4491a4b27f4a66dc93d561a478c005402f3746e8
Shengbo Wang <st169852@stud.uni-stuttgart.de>
2020-12-11 23:14:21 +0100

    update visualization

---
+++ diabetic_retinopathy/visualization.py
@@ -0,0 +1,45 @@
+from gradcam import GradCAM, overlay_gradCAM, get_img_array
+from guided_backpropagation import GuidedBackprop, deprocess_image
+import cv2
+from matplotlib import pyplot as plt
+from models.resnet import resnet50
+
+
+image = get_img_array("./IDRID_dataset/images/test/IDRiD_001.jpg", (256, 256))
+model = resnet50(2)
+model.build(input_shape=(16, 256, 256, 3))
+# #
+# checkpoint = tf.train.Checkpoint(myModel=model)
+# checkpoint.restore(tf.train.latest_checkpoint('./checkpoint/train'))
+# model.compile(optimizer='adam', loss='SparseCategoricalCrossentropy', metrics='SparseCategoricalAccuracy')
+model.summary()
+
+
+# GradCAM
+gradcam = GradCAM(model=model, layerName="conv5_block3_out")
+cam3 = gradcam.compute_heatmap(image=image, classIdx=1, upsample_size=(4288, 2848))
+plt.matshow(cam3)
+plt.title("cam3")
+plt.show()
+original_image = cv2.imread("./IDRID_dataset/images/test/IDRiD_001.jpg")
+cam3 = overlay_gradCAM(original_image, cam3)
+gradcam = cv2.cvtColor(cam3, cv2.COLOR_BGR2RGB)
+plt.matshow(gradcam)
+plt.title("GradCAM")
+plt.show()
+
+# Guided Backpropagation
+GuidedBP = GuidedBackprop(model=model, layerName="conv5_block3_out")
+gb = GuidedBP.guided_backprop(image, upsample_size=(4288, 2848))
+gb_im = deprocess_image(gb)
+gb_im = cv2.cvtColor(gb_im, cv2.COLOR_BGR2RGB)
+plt.matshow(gb_im)
+plt.title("Guided Backpropagation")
+plt.show()
+
+# Guided GradCAM
+guided_gradcam = deprocess_image(gb * cam3)
+guided_gradcam = cv2.cvtColor(guided_gradcam, cv2.COLOR_BGR2RGB)
+plt.matshow(guided_gradcam)
+plt.title("Guided GradCAM")
+plt.show()
