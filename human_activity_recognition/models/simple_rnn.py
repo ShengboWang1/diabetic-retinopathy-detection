6c61952d9f0082174c578415affdbc61c6b675c8
Shengbo Wang <st169852@stud.uni-stuttgart.de>
2021-01-12 15:00:30 +0100

    update rnn

--- human_activity_recognition/models/simple_rnn.py
+++ human_activity_recognition/models/simple_rnn.py
@@ -9,7 +9,7 @@ def simple_rnn(n_neurons, dense_units, dropout_rate, window_size, rnn_units=30):
 
     model = Sequential()
     model.add(layers.LSTM(256, input_shape=(window_size, 6), return_sequences=True))
-    model.add(layers.LSTM(128, input_shape=(window_size, 6), return_sequences=True))
+    # model.add(layers.LSTM(128, input_shape=(window_size, 6), return_sequences=True))
     model.add(layers.LSTM(64, input_shape=(window_size, 6), return_sequences=True))
     # model.add(layers.Bidirectional(layers.LSTM(n_neurons, input_shape=(window_size, 6), return_sequences=True)))
     # model.add(layers.Bidirectional(layers.LSTM(32, input_shape=(n_neurons, 6), return_sequences=True)))
