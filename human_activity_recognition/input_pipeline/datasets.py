fec7939266ada8917e11ea85ef4e64d0d28e6e30
Shengbo Wang <st169852@stud.uni-stuttgart.de>
2021-01-12 15:13:58 +0100

    try to prevent overfitting

--- human_activity_recognition/input_pipeline/datasets.py
+++ human_activity_recognition/input_pipeline/datasets.py
@@ -75,7 +75,7 @@ def prepare(ds_train, ds_val, ds_test, batch_size, caching):
     # ds_train = ds_train.map(
     #     augment, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
-    # ds_train = ds_train.shuffle(300)
+    ds_train = ds_train.shuffle(1000)
     # ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples // 10)
     ds_train = ds_train.batch(batch_size)
     ds_train = ds_train.repeat(-1)
