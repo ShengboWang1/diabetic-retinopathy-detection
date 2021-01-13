8c31526b83cf6871f04fb96720bd2120b7ad04cc
Shengbo Wang <st169852@stud.uni-stuttgart.de>
2021-01-13 11:18:41 +0100

    add softmax...

--- human_activity_recognition/models/simple_rnn.py
+++ human_activity_recognition/models/simple_rnn.py
@@ -8,13 +8,13 @@ from tensorflow.keras import layers, Sequential
 def simple_rnn(n_neurons, dense_units, dropout_rate, window_size, rnn_units=30):
 
     model = Sequential()
-    model.add(layers.LSTM(64, input_shape=(window_size, 6), return_sequences=True))
+    model.add(layers.LSTM(256, input_shape=(window_size, 6), return_sequences=True))
     # model.add(layers.LSTM(128, input_shape=(window_size, 6), return_sequences=True))
-    # model.add(layers.LSTM(64, input_shape=(window_size, 6), return_sequences=True))
+    model.add(layers.LSTM(64, input_shape=(window_size, 6), return_sequences=True))
     # model.add(layers.Bidirectional(layers.LSTM(n_neurons, input_shape=(window_size, 6), return_sequences=True)))
     # model.add(layers.Bidirectional(layers.LSTM(32, input_shape=(n_neurons, 6), return_sequences=True)))
     model.add(layers.Dropout(dropout_rate))
     model.add(layers.Dense(dense_units, activation='relu'))
-    model.add(layers.Dense(13))
+    model.add(layers.Dense(13, activation='softmax'))
 
     return model
