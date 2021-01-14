1c57845bd1e0c876ead3c8b1b17980d9cb71b9bd
Shengbo Wang <st169852@stud.uni-stuttgart.de>
2021-01-14 18:05:26 +0100

    add fine tuning and 2 different models

--- human_activity_recognition/models/multi_lstm.py
+++ human_activity_recognition/models/multi_lstm.py
@@ -8,7 +8,7 @@ from tensorflow.keras.layers import Dense, Dropout, LSTM
 def multi_lstm(n_lstm, n_dense, dropout_rate, window_size, lstm_units, dense_units):
     model = Sequential()
     for i in range(1, n_lstm):
-        model.add(LSTM(lstm_units[i-1], input_shape=(window_size, 6), return_sequences=True))
+        model.add(LSTM(lstm_units, input_shape=(window_size, 6), return_sequences=True))
     # model.add(layers.LSTM(128, input_shape=(window_size, 6), return_sequences=True))
     # model.add(LSTM(64, input_shape=(window_size, 6), return_sequences=True))
     # model.add(layers.Bidirectional(layers.LSTM(n_neurons, input_shape=(window_size, 6), return_sequences=True)))
